0461cee58359399c16514c9a42bb9f4c8bd175db
SolutionsANZ <solutionsanz2@gmail.com>
2018-11-23 23:17:27 +1100

    Fixing namesapce, set to apis4gifts

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,7 +2,7 @@
 #microservices..
   #apis4atp..
   #git clone --quiet https://github.com/solutionsanz/apis4atp >>/tmp/noise.out && cd apis4atp
-  kubectl create namespace apis4gifts >>/tmp/noise.out
-  kubectl create -f kubernetes/apis4gifts-dpl.yaml >>/tmp/noise.out
-  kubectl create -f kubernetes/apis4gifts-svc.yaml >>/tmp/noise.out
-  kubectl create -f kubernetes/apis4gifts-ing.yaml >>/tmp/noise.out
+  kubectl create namespace apis4gifts
+  kubectl create -f kubernetes/apis4gifts-dpl.yaml
+  kubectl create -f kubernetes/apis4gifts-svc.yaml
+  kubectl create -f kubernetes/apis4gifts-ing.yaml
