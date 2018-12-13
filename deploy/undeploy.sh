a8933f0b5347a71970656830cc4eb9318d1020dc
SolutionsANZ <solutionsanz2@gmail.com>
2018-12-13 16:14:34 +1100

    Adding namesapce into undeploy script for secret

--- deploy/undeploy.sh
+++ deploy/undeploy.sh
@@ -4,5 +4,5 @@
   kubectl delete -f kubernetes/apis4gifts-ing.yaml
   kubectl delete -f kubernetes/apis4gifts-svc.yaml
   kubectl delete -f kubernetes/apis4gifts-dpl.yaml
-  kubectl delete secret dhreg
+  kubectl delete secret dhreg --namespace=apis4gifts
   kubectl delete namespace apis4gifts
