4121dbb427792368032d1cad7210b150eb0a8415
SolutionsANZ <solutionsanz2@gmail.com>
2018-12-13 16:04:55 +1100

    Adding ability to create secret as part of deployment script

--- deploy/undeploy.sh
+++ deploy/undeploy.sh
@@ -4,4 +4,5 @@
   kubectl delete -f kubernetes/apis4gifts-ing.yaml
   kubectl delete -f kubernetes/apis4gifts-svc.yaml
   kubectl delete -f kubernetes/apis4gifts-dpl.yaml
+  kubectl delete secret dhreg
   kubectl delete namespace apis4gifts
