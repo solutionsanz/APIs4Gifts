92cdd9a2ebe594bb0434d6de34045eca26943722
SolutionsANZ <solutionsanz2@gmail.com>
2018-12-13 16:02:23 +1100

    Adding deploy template script

--- deploy/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#microservices..
-  #apis4atp..
-  #git clone --quiet https://github.com/solutionsanz/apis4atp >>/tmp/noise.out && cd apis4atp
-  kubectl create namespace apis4gifts
-  kubectl create -f kubernetes/apis4gifts-dpl.yaml
-  kubectl create -f kubernetes/apis4gifts-svc.yaml
-  kubectl create -f kubernetes/apis4gifts-ing.yaml
